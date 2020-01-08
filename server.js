4ceaaff8e40a64f68f1a9b52b1a17f5690214c17
imgopz <rrgkmech@gmail.com>
2020-01-08 15:05:11 +0000

    Adding basic routes

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const connectDB = require("./config/db")
 
-app.get('/', (req, res) => console.log("Api is running...!"))
-
 connectDB();
+app.get('/', (req, res) => console.log("Api is running...!"));
+
+//This will help us to follow RESTful routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
