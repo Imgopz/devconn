507e88089c0b84422abbcc14b5ffbfd19c75418c
imgopz <rrgkmech@gmail.com>
2020-01-23 08:44:43 +0000

    404 cannot get /api/profile/me has been resolved.

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -5,7 +5,7 @@ import { GET_PROFILE, PROFILE_ERROR } from './types'
 // Get current users profile
 export const getCurrentProfile = () => async dispatch => {
 	try{
-		const res = await axios.get('/api/profile/me');
+		const res = await axios.get('/api/profile/me', { validateStatus: false });
 		
 		dispatch({
 			type: GET_PROFILE,
