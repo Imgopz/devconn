94531d2699e6efd747fa08be1f7b8d4964835229
imgopz <rrgkmech@gmail.com>
2020-01-29 03:18:59 +0000

    add exp component

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import { loadUser } from './actions/auth'
 import Dashboard from './components/dashboard/Dashboard'
 import CreateProfile from './components/profile-forms/CreateProfile'
 import EditProfile from './components/profile-forms/EditProfile'
+import AddExperience from './components/profile-forms/AddExperience'
 import PrivateRoute from './components/routing/PrivateRoute'
 
 import './App.css';
@@ -42,6 +43,7 @@ const App = () => {
 						  <PrivateRoute exact path='/dashboard' component={Dashboard} />
 						  <PrivateRoute exact path='/create-profile' component={CreateProfile} />
 						  <PrivateRoute exact path='/edit-profile' component={EditProfile} />
+						  <PrivateRoute exact path='/add-experience' component={AddExperience} />
 					  </Switch>
 				  </section>
 			  </Fragment>
