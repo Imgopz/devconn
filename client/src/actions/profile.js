b6e24e0616ddd60484f1ebf60c0e738e5efc81d4
imgopz <rrgkmech@gmail.com>
2020-01-29 06:42:21 +0000

    Finishing profile actions & Reducer Lecture 53

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { setAlert } from './alert'
-import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE, ACCOUNT_DELETED, CLEAR_PROFILE } from './types'
+import { GET_PROFILE, GET_PROFILES, GET_REPOS, PROFILE_ERROR, UPDATE_PROFILE, ACCOUNT_DELETED, CLEAR_PROFILE } from './types'
 
 // Get current users profile
 export const getCurrentProfile = () => async dispatch => {
@@ -9,6 +9,63 @@ export const getCurrentProfile = () => async dispatch => {
 		
 		dispatch({
 			type: GET_PROFILE,
+			payload: res.data
+		})
+		
+	} catch (err){
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		})
+	}
+}
+
+// Get profiles
+export const getProfiles = () => async dispatch => {
+	dispatch({ type: CLEAR_PROFILE })
+	try{
+		const res = await axios.get('/api/profile');
+		
+		dispatch({
+			type: GET_PROFILES,
+			payload: res.data
+		})
+		
+	} catch (err){
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		})
+	}
+}
+
+// Get profile by id
+export const getProfileById = userId => async dispatch => {
+
+	try{
+		const res = await axios.get(`/api/profile/user/{userId}`);
+		
+		dispatch({
+			type: GET_PROFILE,
+			payload: res.data
+		})
+		
+	} catch (err){
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		})
+	}
+}
+
+// Get github repos
+export const getGithubRepos = username => async dispatch => {
+
+	try{
+		const res = await axios.get(`/api/profile/github/{username}`);
+		
+		dispatch({
+			type: GET_REPOS,
 			payload: res.data
 		})
 		
