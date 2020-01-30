198ec83dc504dd5d6e413bd8bb53dede1285b5aa
imgopz <rrgkmech@gmail.com>
2020-01-30 07:02:16 +0000

    Display profiles

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 	
 	const authLinks = (
 		<ul>
+			<li><Link to="/profiles">Developers</Link></li>
 			<li>
 				<Link to='/dashboard'>
 					<i className="fa fa-user" />{' '}
@@ -25,7 +26,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 	
 	const guestLinks = (
 		<ul>
-			<li><Link to="#!">Developers</Link></li>
+			<li><Link to="/profiles">Developers</Link></li>
 			<li><Link to="/register">Register</Link></li>
 			<li><Link to="/login">Login</Link></li>
 	  	</ul>
