1cc5fc9ffb56a9904048bf475f76532191753aca
imgopz <rrgkmech@gmail.com>
2020-01-31 17:08:31 +0000

    profile top and about

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import { getProfileById } from '../../actions/profile'
+import ProfileTop from './ProfileTop'
+import ProfileAbout from './ProfileAbout'
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
 	
 	useEffect(()=> {
 		getProfileById(match.params.id);
-	}, [getProfileById])
+	}, [getProfileById, match.params.id])
 	
 	return(
 		<Fragment>
@@ -19,6 +21,10 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
 				{ auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (
 					<Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>
 				)}
+				<div class="profile-grid my-1">
+					<ProfileTop profile={profile} />
+					<ProfileAbout profile={profile} />
+				</div>
 				</Fragment> }
 		</Fragment>
 	)
